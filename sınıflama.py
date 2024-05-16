0b7e5dd523d094f47473ffe15923870f6c9832ea
ASUSS <kzlkayasyfllh@gmail.com>
2024-05-16 14:50:17 +0300

    1.1

--- sınıflama.py
+++ sınıflama.py
@@ -1,40 +1,20 @@
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing.image import load_img, ImageDataGenerator
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Dropout, Flatten
-import PIL
-import PIL.Image
-
-# dosya yolu
-toplam_res = 0
-dosya_yolu ="model"
-dirs = os.listdir(dosya_yolu)
-for dir in dirs:
-     dosya = list(os.listdir('model/'+dir))
+import glob
 
 
-     #toplam_res = toplam_res + len(dosya)
-     # dosya kontrol için
-     #print(dir + ' dosyasında  ' + str(len(dosya)) + ' resim ')
-     #print(str(toplam_res) + ' resim
-"""cicekler = [os.path.join(dosya_yolu, "Lale", f) for f in os.listdir(os.path.join(dosya_yolu, "Lale"))]
-img = PIL.Image.open(cicekler[73])
-img.show()"""
 
 
-batch_size = 32
-img_height = 180
-img_width = 180
-# TensorFlow ile veri kümesini yükleme
-train_ds = tf.keras.utils.image_dataset_from_directory(dosya_yolu,
-                                                       validation_split=0.2,
-                                                       subset="training",
-                                                       seed=123,
-                                                       image_size=(img_height,img_width),
-                                                       batch_size=batch_size)
+folder_dir = 'model/*'
+
+for images in glob.iglob(f'{folder_dir}/*'):
+
+    if (images.endswith(".jpg")):
+        #print(images)
+
+
+
+
+
+
+
+
 
