8e9ae013d821b4ada4d61eb0bc43493f084ff385
ASUSS <kzlkayasyfllh@gmail.com>
2024-05-17 00:33:43 +0300

    1.2

--- sınıflama.py
+++ sınıflama.py
@@ -1,20 +1,22 @@
-import glob
+import cv2
+from PIL import Image
+import os
 
 
 
-
-folder_dir = 'model/*'
-
-for images in glob.iglob(f'{folder_dir}/*'):
-
-    if (images.endswith(".jpg")):
-        #print(images)
-
-
-
-
-
-
-
+data_dir = 'model'
+data_list = os.listdir(os.path.join(data_dir))
+#print(data_list)
+for image_class in os.listdir(data_dir):
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try:
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in ['jpeg', 'jpg', 'png']:
+                print('resim ext liste yok {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e:
+            print('Issue with image {}'.format(image_path))
 
 
